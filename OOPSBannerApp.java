2e61ecfce268918a079509b31797cfd3631bf1fc
pratyushk191 <pratyushk191@gmail.com>
2026-02-17 22:37:30 +0530

    Coded UC2 to Print OOPS Banner using + operator

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,13 @@
-/**
- * OOPSBannerApp
- * 
- * Prints OOPS to console.
- * 
- * @author YourName
- * @version 1.0
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("   * *       **        **          ****  ");
+        System.out.println(" *     *   *     *   *     *     ** ");
+        System.out.println(" *     *   *     *   *     *   **   ");
+        System.out.println(" *     *   *     *   ** ***       **");
+        System.out.println(" *     *   *     *   *             *** ");
+        System.out.println(" *     *   *     *   *               **  ");
+        System.out.println("    **        **     *            ****  ");
     }
 }
